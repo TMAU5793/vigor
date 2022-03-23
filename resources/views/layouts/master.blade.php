a909c57aa3cdea7480ae7852662f0a8bb414a428
wriy9o <thank@grasp.asia>
2022-03-23 19:01:54 +0700

    start app

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ url('assets/bootstrap-5/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/fonts/FCIconic/stylesheet.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/main.css') }}">
+</head>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
